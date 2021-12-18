650440885d8687462394771129f343d66227879e
admin <admin@example.com>
2021-12-18 15:55:34 +0800

    add mkimage
    Change-Id: I09837505579a58729bc3d47022990600ecdf353d

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 now_path=$(pwd)
 export now_path
 echo $now_path
-export PATH=$now_path/jdk1.6.0_30/bin:$PATH
-export PATH=$now_path/4.4.6/bin:$PATH
-export PATH=$now_path/make-3.81/bin:$PATH
+export PATH=$now_path/../jdk1.6.0_30/bin:$PATH
+export PATH=$now_path/../4.4.6/bin:$PATH
+export PATH=$now_path/../make-3.81/bin:$PATH
+export PATH=$now_path/usr/local/sbin/:$PATH
